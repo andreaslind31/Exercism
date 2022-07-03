8cee310e87f1efaf62b4216f66ca145de6931487
Andreas Lind <andreas.lind@fasttrack-solutions.com>
2022-07-03 22:25:06 +0200

    beauty exercise

--- go/booking-up-for-beauty/booking_up_for_beauty.go
+++ go/booking-up-for-beauty/booking_up_for_beauty.go
@@ -1,6 +1,7 @@
 package booking
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -23,15 +24,25 @@ func HasPassed(date string) bool {
 
 // IsAfternoonAppointment returns whether a time is in the afternoon
 func IsAfternoonAppointment(date string) bool {
-	panic("Please implement the IsAfternoonAppointment function")
+	layout := "Monday, January 2, 2006 15:04:05"
+	parsedTime, _ := time.Parse(layout, date)
+
+	return parsedTime.Hour() >= 12 && parsedTime.Hour() < 18
 }
 
 // Description returns a formatted string of the appointment time
 func Description(date string) string {
-	panic("Please implement the Description function")
+	schedule := Schedule(date)
+
+	return fmt.Sprintf("You have an appointment on %v, %v %d, %d, at %d:%d.", schedule.Weekday(), schedule.Month(), schedule.Day(), schedule.Year(), schedule.Hour(), schedule.Minute())
 }
 
 // AnniversaryDate returns a Time with this year's anniversary
 func AnniversaryDate() time.Time {
-	panic("Please implement the AnniversaryDate function")
+	yearNow := time.Now().Year()
+	dateWithAddedYear := fmt.Sprintf("%d-09-15", yearNow)
+	layout := "2006-01-02"
+	anniversary, _ := time.Parse(layout, dateWithAddedYear)
+
+	return anniversary
 }
